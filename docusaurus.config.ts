d0828217b44bd3833980943631334905ea88c619
dev024 <developer024h@gmail.com>
2024-08-17 15:30:46 +0800

    接入搜索组件，将视频文件分离至oss

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -69,6 +69,38 @@ const config: Config = {
   themeConfig: {
     // Replace with your project's social card
     image: "img/docusaurus-social-card.jpg",
+    algolia: {
+      // The application ID provided by Algolia
+      appId: "U4DH6S3C9R",
+
+      // Public API key: it is safe to commit it
+      apiKey: "df8d5a3f390314483794e835497676e5",
+
+      indexName: "passnat",
+
+      // Optional: see doc section below
+      contextualSearch: true,
+
+      // Optional: Specify domains where the navigation should occur through window.location instead on history.push. Useful when our Algolia config crawls multiple documentation sites and we want to navigate with window.location.href to them.
+      // externalUrlRegex: "external\\.com|domain\\.com",
+
+      // Optional: Replace parts of the item URLs from Algolia. Useful when using the same search index for multiple deployments using a different baseUrl. You can use regexp or string in the `from` param. For example: localhost:3000 vs myCompany.com/docs
+      replaceSearchResultPathname: {
+        from: "/docs/", // or as RegExp: /\/docs\//
+        to: "/"
+      },
+
+      // Optional: Algolia search parameters
+      searchParameters: {},
+
+      // Optional: path for search page that enabled by default (`false` to disable it)
+      searchPagePath: "search",
+
+      // Optional: whether the insights feature is enabled or not on Docsearch (`false` by default)
+      insights: false
+
+      //... other Algolia params
+    },
     navbar: {
       title: "PassNAT",
       logo: {
